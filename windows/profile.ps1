33ddc7074851c00557f9de50bb30fa36faf8b1c0
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2022-07-03 22:30:36 +0000

    Update profile.ps1

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -5,3 +5,4 @@ Import-Module z
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
