9fa124538c44f3a92868f3765cb4d16b3c096acc
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-04-02 08:40:04 +0200

    Update dailywallpaper.ps1

--- windows/dailywallpaper.ps1
+++ windows/dailywallpaper.ps1
@@ -1,5 +1,7 @@
 $result = Invoke-RestMethod -Uri "https://api.nasa.gov/planetary/apod?api_key=RAarp1XLTlplmQ0FywIKKAXcVbUHkinYmRYB9tYl" -Headers @{"Accept"="*/*"}
 Invoke-WebRequest $result.hdurl -OutFile $ENV:UserProfile\wallpaper.jpg
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d $ENV:UserProfile\wallpaper.jpg /f
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "AutoColorization" -Value 0
 Start-Sleep -s 10
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters 1, True
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "AutoColorization" -Value 1
