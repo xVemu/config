bb80072909d9dac0056209b71286b2435dc5f97a
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-08-02 14:10:18 +0200

    Update profile.ps1

--- windows/files/profile.ps1
+++ windows/files/profile.ps1
@@ -1,8 +1,8 @@
 oh-my-posh init pwsh --config ~/cobalt2.omp.json | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
-Import-Module z
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
