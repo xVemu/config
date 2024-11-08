cd66a5a28c1048f8a7f7814c3c333d3b8abcc588
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2024-11-08 21:15:02 +0100

    Update profile.ps1

--- windows/files/profile.ps1
+++ windows/files/profile.ps1
@@ -6,3 +6,4 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+if (-not (Test-Path -Path $PROFILE)) { New-Item -Type File -Path $PROFILE -Force }; Add-Content -Path $PROFILE -Value 'Invoke-Expression "$(vfox activate pwsh)"'
