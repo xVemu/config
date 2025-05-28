f2c8629d50d76eb3dc74980a8d3100277f4e293f
Kamil Kras <38427679+xVemu@users.noreply.github.com>
2025-05-28 09:17:19 +0200

    Add macos

---
+++ macos/config.fish
@@ -0,0 +1,19 @@
+if status is-interactive
+    alias cat bat
+    alias cp 'cp -riv'
+    alias l 'eza -l'
+    alias la 'eza -a'
+    alias ll 'eza -l'
+    alias lla 'eza -la'
+    alias ls eza
+    alias lt 'eza --tree'
+    alias mkdir 'mkdir -vp'
+    alias mv 'mv -iv'
+    alias vi nvim
+
+    /Users/kkras/.nix-profile/bin/zoxide init fish | source
+    vfox activate fish | source
+
+    set -g theme_hide_hostname yes
+    set -g fish_prompt_pwd_dir_length 0
+end
